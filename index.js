233dd9e1ea858c7348ce9f0cf8af93d536c563ba
sega <sergohack2015@gmail.com>
2024-03-06 22:32:07 +0200

    Enhance Markdown Formatting
    This commit introduces improvements to Markdown formatting in Node.js. The following changes were made:
    - Bold text ( ** ) is now wrapped in <b> tags.
    - Italicized text (__) is enclosed in <i> tags.
    - Preformatted code ( ``` ``` ) blocks are transformed using <pre> tags.
    - Monospaced font (` `) is represented with <tt> tags.
    - Paragraphs are wrapped in <p> tags.

--- index.js
+++ index.js
@@ -6,6 +6,18 @@ const markdownText = fs.readFileSync('input.md', 'utf8');
 // Функція для перетворення Markdown на HTML
 function convertMarkdownToHtml(markdownText) {
 
+    // Замінюємо жирний текст
+    markdownText = markdownText.replace(/\*\*(.*?)\*\*/g, '<b>$1</b>');
+    // Замінюємо курсив
+    markdownText = markdownText.replace(/\_\s?([^\n]+)\_/g, '<i>$1</i>');
+    // Замінюємо преформатований текст
+    markdownText = markdownText.replace(/```([^`]+)```/g, '<pre>$1</pre>');
+    // Замінюємо моноширинний шрифт
+    markdownText = markdownText.replace(/`([^`]+)`/g, '<tt>$1</tt>');
+    // Замінюємо параграфи
+    markdownText = markdownText.replace(/([^\n]+\n?)/g, '\n<p>$1</p>\n');
+
+    return markdownText;
 
 }
 
